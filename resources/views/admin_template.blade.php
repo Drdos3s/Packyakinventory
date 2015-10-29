4fdb247ca18a1b514515fc1df1efd47510476205
p8ntball35 <kraley@rams.colostate.edu>
2015-10-29 12:49:48 -0600

    More work with getting values from son response. Can start moving forward with formatting data as we have all the information we need to pull in the items and inventory.

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -16,10 +16,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
         <link href="{{ asset("/bower_components/admin-lte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
-        <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-              page. However, you can choose any other skin. Make sure you
-              apply the skin class to the body tag so the changes take effect.
-        -->
         <link href="{{ asset("/bower_components/admin-lte/dist/css/skins/skin-green.min.css")}}" rel="stylesheet" type="text/css" />
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -56,45 +52,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- Main content -->
             <section class="content">
                 <!-- Your Page Content Here -->
-                <?php
-
-
-/*$access_token = 'KI0ethBHis2N76q1jyYung';
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_HTTPHEADER, array( 'Authorization: Bearer ' . $access_token, 'Content-Type: application/json', 'Accept: application/json'));
-curl_setopt($curl, CURLOPT_URL, 'https://connect.squareup.com/v1/2AT576X1MSDJ9/payments?begin_time=2015-10-24T00:00:00Z&end_time=2015-10-25T00:00:00Z&limit=1');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-
-$json = curl_exec($curl);
-curl_close($curl);
-echo $json;
-echo '-------------------------------------------------------';
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Bearer KI0ethBHis2N76q1jyYung', 'Content-Type: application/json', 'Accept: application/json'));
-    curl_setopt ($curl, CURLOPT_URL, "https://connect.squareup.com/v1/2AT576X1MSDJ9/items/");
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-    if(!curl_exec($curl)){
-        die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
-    }
-    $ch = curl_exec ($curl);
-    curl_close ($curl);
-    $ch = json_decode($ch, true);
-    $c = count($ch);
-    echo $c;*/
-    
-
-
-/*$access_token = 'KI0ethBHis2N76q1jyYung';
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_HTTPHEADER, array( 'Authorization: Bearer ' . $access_token, 'Content-Type: application/json', 'Accept: application/json'));
-curl_setopt($curl, CURLOPT_URL, 'https://connect.squareup.com/v1/2AT576X1MSDJ9/items');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-
-$json = curl_exec($curl);
-curl_close($curl);
-echo $json;*/
+                
+<?php
 
 require_once base_path('vendor/mashape/unirest-php/src/Unirest.php');
 
@@ -140,7 +99,7 @@ function createItem() {
       )
     )
   );
-  $response = Unirest\Request::post($connectHost . '/v1/me/items', $requestHeaders, json_encode($request_body));
+  $response = Unirest\Request::post($connectHost . '/v1/me/items/', $requestHeaders, json_encode($request_body));
   echo $response->code;
   echo json_encode($response->body, JSON_PRETTY_PRINT);
   if ($response->code == 200) {
@@ -169,11 +128,18 @@ function checkItemNum() {
 
 
   $items = array();
-  $response = Unirest\Request::get($connectHost . '/v1/3526BMVFNJZZX/items', $requestHeaders);
+  $response = Unirest\Request::get($connectHost . '/v1/3526BMVFNJZZX/items/0063a336-8260-4455-b489-1dba4da0859b', $requestHeaders);
 
-  echo json_encode($response->body, JSON_PRETTY_PRINT);
+  $json = json_encode($response->body);
 
-  $items = array_merge($items, $response->body);
+  $test = json_decode($json, true);
+
+  var_dump($test);
+
+
+  echo $test['category']['id'];
+
+  //$items = array_merge($items, $response->body);
 
   //echo count($items);
 }
@@ -203,24 +169,8 @@ function getInventory() {
 
   //echo count($inventory);
 }
-getInventory();
+//getInventory();
 
-/*# Updates the Milkshake item to rename it to "Malted Milkshake"
-function updateItem($itemId) {
-  global $accessToken, $connectHost, $requestHeaders;
-  $request_body = array(
-    "name"=>"Malted Milkshake"
-  );
-  $response = Unirest\Request::put($connectHost . '/v1/me/items/' . $itemId, $requestHeaders, json_encode($request_body));
-  if ($response->code == 200) {
-    error_log('Successfully updated item:');
-    error_log(json_encode($response->body, JSON_PRETTY_PRINT));
-    return $response->body;
-  } else {
-    error_log('Item update failed');
-    return NULL;
-  }
-}*/
 #Deletes the Malted Milkshake item.
 function deleteItem($itemId) {
   $access_token = 'KI0ethBHis2N76q1jyYung';
@@ -242,129 +192,7 @@ function deleteItem($itemId) {
   }
 }
 //deleteItem('e9407738-314a-4b22-950a-a6ba95331f2e');
-
-
-//-----------------GET PAYMENTS WORKING EXAMPLE-----------------
-function formatMoney($money) {
-  return money_format('%+.2n', $money / 100);
-}
-
-function get2014Payments() {
-  global $accessToken;
-
-  $connectHost = 'https://connect.squareup.com';
-  # Restrict the request to the 2014 calendar year, eight hours behind UTC
-  # Make sure to URL-encode all parameters
-  $parameters = http_build_query(
-    array(
-      'begin_time' => '2015-01-01T00:00:00-08:00',
-      'end_time'   => '2015-01-02T00:00:00-08:00'
-    )
-  );
-
-  // GOOD echo $parameters;
-  # Standard HTTP headers for every Connect API request
-  $requestHeaders = array (
-    'Authorization' => 'Bearer KI0ethBHis2N76q1jyYung',
-    'Accept' => 'application/json',
-    'Content-Type' => 'application/json'
-  );
-
-  // GOOD echo $requestHeaders['Authorization'];
-  
-  $payments = array();
-  $requestPath = $connectHost . '/v1/me/payments?begin_time=2015-10-20T00:00:00Z&end_time=2015-10-21T00:00:00Z&limit=200';
-
-  // GOOD echo $requestPath;
-
-  $response = Unirest\Request::get($requestPath, $requestHeaders);
-  
-  // GOOD 200 echo $response -> code;     // Headers
-
-  $payments = array_merge($payments, $response->body);
-
-  // GOOD 77 echo count($payments);
-
-  $seenPaymentIds = array();
-  $uniquePayments = array();
-  foreach ($payments as $payment) {
-    if (array_key_exists($payment->id, $seenPaymentIds)) {
-      continue;
-    }
-    $seenPaymentIds[$payment->id] = true;
-    array_push($uniquePayments, $payment);
-  }
-
-  return $uniquePayments;
-  /*$moreResults = true;
-  while ($moreResults) {
-    # Send a GET request to the List Payments endpoint
-    $response = Unirest\Request::get($requestPath, $requestHeaders);
-    # Read the converted JSON body into the cumulative array of results
-    $payments = array_merge($payments, $response->body);
-    # Check whether pagination information is included in a response header, indicating more results
-    if (array_key_exists('Link', $response->headers)) {
-      $paginationHeader = $response->headers['Link'];
-      if (strpos($paginationHeader, "rel='next'") !== false) {
-        # Extract the next batch URL from the header.
-        #
-        # Pagination headers have the following format:
-        # <https://connect.squareup.com/v1/MERCHANT_ID/payments?batch_token=BATCH_TOKEN>;rel='next'
-        # This line extracts the URL from the angle brackets surrounding it.
-        $requestPath = explode('>', explode('<', $paginationHeader)[1])[0];
-      } else {
-        $moreResults = false;
-      }
-    } else {
-      $moreResults = false;
-    }*/
-}
-
-function printSalesReport($payments) {
-  # Variables for holding cumulative values of various monetary amounts
-  $collectedMoney = $taxes = $tips = $discounts = $processingFees = 0;
-  $returned_processingFees = $netMoney = $refunds = 0;
-  # Add appropriate values to each cumulative variable
-  foreach ($payments as $payment) {
-    $collectedMoney  = $collectedMoney  + $payment->total_collected_money->amount;
-    $taxes           = $taxes           + $payment->tax_money->amount;
-    $tips            = $tips            + $payment->tip_money->amount;
-    $discounts       = $discounts       + $payment->discount_money->amount;
-    $processingFees  = $processingFees  + $payment->processing_fee_money->amount;
-    $netMoney        = $netMoney        + $payment->net_total_money->amount;
-    $refunds         = $refunds         + $payment->refunded_money->amount;
-    # When a refund is applied to a credit card payment, Square returns to the merchant a percentage 
-    # of the processing fee corresponding to the refunded portion of the payment. This amount
-    # is not currently returned by the Connect API, but we can calculate it as shown:
-    # If a processing fee was applied to the payment AND some portion of the payment was refunded...
-    if ($payment->processing_fee_money->amount < 0 and $payment->refunded_money->amount < 0) {
-      # ...calculate the percentage of the payment that was refunded...
-      $percentage_refunded = $payment->refunded_money->amount / (float)$payment->total_collected_money->amount;
-      # ...and multiply that percentage by the original processing fee
-      $returned_processingFees = $returned_processingFees + ($payment->processing_fee_money->amount * $percentage_refunded);
-    }
-  }
-  # Calculate the amount of pre-tax, pre-tip money collected
-  $basePurchases = $collectedMoney - $taxes - $tips;
-  # Print a sales report similar to the Sales Summary in the merchant dashboard.
-  echo '<pre>';
-  echo '==SALES REPORT FOR 2014==' . '<br/>';
-  echo 'Gross Sales:       ' . formatMoney($basePurchases - $discounts) . '<br/>';
-  echo 'Discounts:         ' . formatMoney($discounts) . '<br/>';
-  echo 'Net Sales:         ' . formatMoney($basePurchases) . '<br/>';
-  echo 'Tax collected:     ' . formatMoney($taxes) . '<br/>';
-  echo 'Tips collected:    ' . formatMoney($tips) . '<br/>';
-  echo 'Total collected:   ' . formatMoney($basePurchases + $taxes + $tips) . '<br/>';
-  echo 'Fees:              ' . formatMoney($processingFees) . '<br/>';
-  echo 'Refunds:           ' . formatMoney($refunds) . '<br/>';
-  echo 'Fees returned:     ' . formatMoney($returned_processingFees) . '<br/>';
-  echo 'Net total:         ' . formatMoney($netMoney + $refunds + $returned_processingFees) . '<br/>';
-  echo '</pre>';
-}
-
-//$payments = get2014Payments();
-//printSalesReport($payments);
-                ?> 
+?> 
                 @yield('content')
             </section><!-- /.content -->
         </div><!-- /.content-wrapper -->
@@ -377,7 +205,7 @@ function printSalesReport($payments) {
     <!-- REQUIRED JS SCRIPTS -->
 
     <!-- jQuery 2.1.3 -->
-    <script src="{{ asset ("/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.3.min.js") }}"></script>
+    <script src="{{ asset ("/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js") }}"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="{{ asset ("/bower_components/admin-lte/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
     <!-- AdminLTE App -->
