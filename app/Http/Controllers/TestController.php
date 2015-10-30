31930afef491b452a93f8b47042c4389dea6b88e
p8ntball35 <kraley@rams.colostate.edu>
2015-10-29 18:23:35 -0600

    Working to try and require the unrest package in the controller.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,39 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller {
 
+    function checkItemNum() {
+          require_once base_path('vendor/mashape/unirest-php/src/Unirest.php');
+          $access_token = 'KI0ethBHis2N76q1jyYung';
+          $connectHost = 'https://connect.squareup.com';
+          $requestHeaders = array (
+            'Authorization' => 'Bearer ' . $access_token,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+          );
+
+          //DEN -> 1H5A5ZGP2T4DA
+          //PHX -> 3526BMVFNJZZX
+          //OUT -> 9SQD525GSB3T3
+
+
+          $items = array();
+          $response = Unirest\Request::get($connectHost . '/v1/3526BMVFNJZZX/items/0063a336-8260-4455-b489-1dba4da0859b', $requestHeaders);
+
+          $json = json_encode($response->body);
+
+          $test = json_decode($json, true);
+
+          //var_dump($test);
+
+
+          echo $test['category']['id'];
+
+          //$items = array_merge($items, $response->body);
+
+          //echo count($items);
+        }
+        
+
     public function index() {
         $data['tasks'] = [
                 [
@@ -86,6 +119,7 @@ class TestController extends Controller {
                 ]
 
         ];
+        echo dirname(__FILE__);
         return view('test')->with($data);
     }
 
