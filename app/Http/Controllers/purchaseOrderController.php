0c85c09286fde32e65d79deb3460abc04778eda6
p8ntball35 <kraley@rams.colostate.edu>
2016-03-23 15:28:40 -0600

    This is to clean up the code and get ready for finishing and adding to PO

--- app/Http/Controllers/purchaseOrderController.php
+++ app/Http/Controllers/purchaseOrderController.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Client;
 use App\purchaseOrder;
 use App\purchaseOrderItem;
 use App\ItemCategory;
-//use App\createNewItem
+
 use DB;
 use Auth;
 
@@ -19,86 +19,6 @@ use Auth;
 
 class purchaseOrderController extends Controller
 {
-    public function index(){
-        //getting all open purchase orders and converting object to array
-        $existingPurchaseOrders = json_decode(
-                                    json_encode(
-                                        DB::table('purchase_orders')->get()
-                                    ),true);
-        //getting all locations to dynamically populate locations lists used for create items and create purchase orders
-         $existingLocations = json_decode( 
-                                json_encode(
-                                    DB::table('locations')->get()
-                                ),true);
-
-        $purchaseOrderDetailsWithItems = [];
-
-        //creating the batch call for item categories to populate the DB
-        $itemCategoriesAllLocations = [];
-
-        //setting up batch body for the categories request
-        foreach ($existingLocations as $singleLocation) {
-            $formattedCategoryRequest = array('method' => 'GET',
-                                              'relative_path' => '/v1/'.$singleLocation['squareID'].'/categories',
-                                              'access_token' => 'KI0ethBHis2N76q1jyYung',
-                                              'request_id' => $singleLocation['squareID']
-                                              );
-            array_push($itemCategoriesAllLocations, $formattedCategoryRequest);
-        }
-
-        $categoryRequestBatch = array('requests' => $itemCategoriesAllLocations); 
-
-        //initialize new client
-        $client = new Client();
-
-        $categoriesBatch = $client->request('POST', 'https://connect.squareup.com/v1/batch', [
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json'
-                ], 'body' => json_encode($categoryRequestBatch)
-        ]);
-
-        $categoriesList = json_decode($categoriesBatch->getBody(), true);
-
-        //write each category to the database
-        foreach($categoriesList as $categoryList){
-            foreach($categoryList['body'] as $categoryNameAndID){
-                //var_dump($categoryNameAndID);
-                //echo $categoryList['request_id'];
-                
-                $newCategoryInDB = ItemCategory::firstOrNew(['locationID' => $categoryList['request_id'], 'categoryName' => $categoryNameAndID['name']]);
-                $newCategoryInDB -> locationID = $categoryList['request_id'];
-                $newCategoryInDB -> categoryName = $categoryNameAndID['name'];
-                $newCategoryInDB -> categoryID = $categoryNameAndID['id'];
-                $newCategoryInDB -> save();
-            }  
-        }
-
-        //get all the items within a purchase order
-        foreach($existingPurchaseOrders as $existingPurchaseOrder){
-            
-            $existingPurchaseOrder['po_items'] = json_decode(json_encode(DB::table('purchase_order_items')
-                                                ->leftJoin('inventoryList', 'purchase_order_items.purchaseOrderItemVariationID', '=', 'inventoryList.itemVariationID')
-                                                //->select('purchaseOrderItemVariationID')
-                                                ->where('purchaseOrderID', '=', $existingPurchaseOrder['id'])
-                                                ->get()),true);
-
-
-            //var_dump($existingPurchaseOrder);
-            array_push($purchaseOrderDetailsWithItems, $existingPurchaseOrder);
-        }      
-
-        //sending the names of the uniquecategoires to the view for the select button
-        $itemCategoryList = json_decode(json_encode(DB::table('item_categories')
-                                ->select('categoryName')
-                                ->distinct()->get()), true);
-
-        //var_dump($itemCategoryList);
-
-        //var_dump($purchaseOrderDetailsWithItems);
-        return view('purchaseOrder', ['existingPurchaseOrders' => $purchaseOrderDetailsWithItems, 'existingLocations' => $existingLocations, 'categoryList' => $itemCategoryList]);
-    }
-
     public function ajaxRoute(){
         if(Request::ajax()) {
             //direct the ajax request to where it needs to go
@@ -125,31 +45,87 @@ class purchaseOrderController extends Controller
             }
         }
     }
+
+    public function index(){
+        //creating the array for item categories to populate the DB
+        $itemCategoriesAllLocations = [];
+        //purchase order array
+        $purchaseOrderDetailsWithItems = [];
+
+        $existingPurchaseOrders = json_decode(
+                                    json_encode(
+                                        DB::table('purchase_orders')->get()
+                                    ),true);
+        //getting all locations to dynamically populate locations lists used for create items and create purchase orders
+         $existingLocations = json_decode( 
+                                json_encode(
+                                    DB::table('locations')->get()
+                                ),true);
+
+        //setting up batch body for the categories request -- Could probably get away with putting into a new function with logic
+        foreach ($existingLocations as $singleLocation) {
+            $formattedCategoryRequest = array('method' => 'GET',
+                                              'relative_path' => '/v1/'.$singleLocation['squareID'].'/categories',
+                                              'access_token' => 'KI0ethBHis2N76q1jyYung',
+                                              'request_id' => $singleLocation['squareID']
+                                              );
+            array_push($itemCategoriesAllLocations, $formattedCategoryRequest);
+        }
+        $categoryRequestBatch = array('requests' => $itemCategoriesAllLocations); 
+
+        //initialize new client
+        $client = new Client();
+
+        //retrieve all categories
+        $categoriesBatch = $client->request('POST', 'https://connect.squareup.com/v1/batch', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json'
+                ], 'body' => json_encode($categoryRequestBatch)
+        ]);
+
+        $categoriesList = json_decode($categoriesBatch->getBody(), true);
+
+        //write each category to the database
+        foreach($categoriesList as $categoryList){
+            foreach($categoryList['body'] as $categoryNameAndID){        
+                $newCategoryInDB = ItemCategory::firstOrNew(['locationID' => $categoryList['request_id'], 'categoryName' => $categoryNameAndID['name']]);
+                $newCategoryInDB -> locationID = $categoryList['request_id'];
+                $newCategoryInDB -> categoryName = $categoryNameAndID['name'];
+                $newCategoryInDB -> categoryID = $categoryNameAndID['id'];
+                $newCategoryInDB -> save();
+            }  
+        }
+
+        //get all the items within a purchase order
+        foreach($existingPurchaseOrders as $existingPurchaseOrder){
+            $existingPurchaseOrder['po_items'] = json_decode(json_encode(DB::table('purchase_order_items')
+                                                ->leftJoin('inventoryList', 'purchase_order_items.purchaseOrderItemVariationID', '=', 'inventoryList.itemVariationID')
+                                                ->where('purchaseOrderID', '=', $existingPurchaseOrder['id'])
+                                                ->get()),true);
+
+            array_push($purchaseOrderDetailsWithItems, $existingPurchaseOrder);
+        }      
+
+        //sending the names of the unique categoires to the view for the select button
+        $itemCategoryList = json_decode(json_encode(DB::table('item_categories')
+                                ->select('categoryName')
+                                ->distinct()
+                                ->get()), true);
+
+        return view('purchaseOrder', ['existingPurchaseOrders' => $purchaseOrderDetailsWithItems, 'existingLocations' => $existingLocations, 'categoryList' => $itemCategoryList]);
+    }
 }
 
 function createNewPurchaseOrder(){
     //retriving variables from modal form
     $action = $_POST['action'];
-    $po_name = $_POST['po_name'];
-    $po_status = $_POST['po_status'];
-    $po_vendor = $_POST['po_vendor'];
-    $po_location = $_POST['po_location'];
-    $po_invoice_number = $_POST['po_invoice_number'];
-
-    /*echo 'Values being passed with ajax';
-    echo 'po_name: '.$po_name;
-    echo ' po_status: '.$po_status;
-    echo ' po_vendor: '.$po_vendor;
-    echo ' po_location: '.$po_location;
-    echo 'po_invoice set?: '.$po_invoice_number;*/
-    //write to database for new purchase order
-
     $purchaseOrder = new purchaseOrder;
-    $purchaseOrder->po_name = $po_name;
-    $purchaseOrder->po_status = $po_status;
-    $purchaseOrder->po_vendor = $po_vendor;
-    $purchaseOrder->po_invoice_number = $po_invoice_number;
-    $purchaseOrder->po_location = $po_location;
+    $purchaseOrder->po_name = $_POST['po_name'];
+    $purchaseOrder->po_status = $_POST['po_status'];
+    $purchaseOrder->po_vendor = $_POST['po_vendor'];
+    $purchaseOrder->po_invoice_number = $_POST['po_invoice_number'];
+    $purchaseOrder->po_location = $_POST['po_location'];
     $purchaseOrder->save();
 
     $createdPurchaseOrder = json_encode(array('po_name' => $po_name, 'po_status' => $po_status, 'po_vendor' => $po_vendor, 'po_invoice_number' => $po_invoice_number, 'po_location' => $po_location));
@@ -160,18 +136,14 @@ function createNewPurchaseOrder(){
 function addItemToPurchaseOrder(){
     //retrieve data from POST
     $action = $_POST['action'];
-    $selectedPurchaseOrder = $_POST['selectedPurchaseOrder'];
-    $itemVariationID = $_POST['itemVariationID'];
-    $purchaseOrderID = $_POST['packyakPurchaseOrderID'];
 
     //set up new instance for purchase order item
     $purchaseOrderItem = new purchaseOrderItem;
-    $purchaseOrderItem->purchaseOrderName = $selectedPurchaseOrder;
-    $purchaseOrderItem->purchaseOrderID = $purchaseOrderID;
-    $purchaseOrderItem->purchaseOrderItemVariationID = $itemVariationID;
+    $purchaseOrderItem->purchaseOrderName = $_POST['selectedPurchaseOrder'];
+    $purchaseOrderItem->purchaseOrderID = $_POST['packyakPurchaseOrderID'];
+    $purchaseOrderItem->purchaseOrderItemVariationID = $_POST['itemVariationID'];
 
     $purchaseOrderItem->save();
-
 
     return json_encode(array($action, $selectedPurchaseOrder, $itemVariationID, $purchaseOrderID));
     exit;
@@ -186,14 +158,10 @@ function removeItemFromPO(){
         ->where('purchaseOrderID', '=', $purchaseOrderID)
         ->where('purchaseOrderItemVariationID','=', $itemVariationID)
         ->delete();
-
     exit;
 };
 
 function createNewItem($createdItem){
-
-    //var_dump($createdItem);
-
     //setting variables from the post data from ajax call
     $newItemCategory            = $createdItem->newItemCategory;
     $newItemName                = $createdItem->newItemName;
@@ -203,35 +171,25 @@ function createNewItem($createdItem){
     
     $newItemVariationsDecoded = json_decode(json_encode($newItemVariationsRaw), true);
     $variationsListForSquare = [];
+    $newItemArray = [];
     $access_token = 'KI0ethBHis2N76q1jyYung';
     
     //adds each variation to an array that will be added to other data in order to create item
     foreach($newItemVariationsDecoded as $newItemVariation){
         $formattedVariation = array(
-                'name' => $newItemVariation['newVariationName']/*,
+                'name' => $newItemVariation['newVariationName'],
                 'price_money' => array(
                   'currency_code' => 'USD',
                   'amount' => $newItemVariation['newVariationPrice']
                 ),
                 'track_inventory'=> true,
                 'sku' => $newItemVariation['newVariationSKU'],
-                'inventory_alert_type'=> "LOW_QUANTITY"*/
+                'inventory_alert_type'=> "LOW_QUANTITY"
             );
 
+        //pushes new variation with properties into an array to be formatted into JSON right before request happens
         array_push($variationsListForSquare, $formattedVariation);
     }
-
-    /*$postData = array(
-        'name' => $newItemName,
-        //'category_id' => '67c8e187-45af-4795-ba56-985f88051453',
-        'variations' => $variationsListForSquare
-    );*/
-
-    //var_dump($postData);
-
-    
-
-    $newItemArray = [];
 
     foreach($newItemLocationSoldAt as $newSquareItemLocation){
         //get the location info that is needed
@@ -250,90 +208,36 @@ function createNewItem($createdItem){
                                                         ->get()
                                                         ), true);
 
-        //var_dump($categoryIDForSpecificLocation);
-
-
+        //set up general data for each item to once again be converted to json
         $postData = array(
             'name' => $newItemName,
             'category_id' => $categoryIDForSpecificLocation[0]['categoryID'],
             'variations' => $variationsListForSquare
         );
 
-        //$json = json_encode($postData);
-        //echo $json;
-
-        $formattedCreateItemBatchRequest = array('method' => 'POST',
+        //running through and creating each request for the batch in order to create items
+        $formattedCreateItemSingleRequest = array('method' => 'POST',
                                           'relative_path' => '/'.'v1/'.$existingLocation[0]['squareID'].'/items',
                                           'access_token' => 'KI0ethBHis2N76q1jyYung',
                                           'body' => $postData
                                           );
 
-        array_push($newItemArray, $formattedCreateItemBatchRequest);
+        array_push($newItemArray, $formattedCreateItemSingleRequest);
     };
 
+    //Putting in batch request and getting ready to be sent
+    $newItemRequestFullBatch = array('requests' => $newItemArray);
 
-
-
-
-    $newItemRequestBatch = array('requests' => $newItemArray);
-
-    echo json_encode($newItemRequestBatch);
     $client = new Client();
-
-
-    $newItemBatch = $client->request('POST', 'https://connect.squareup.com/v1/batch', [
+    //send the batch request with JSON for each item created per location with variations
+    $newItemBatchResponse = $client->request('POST', 'https://connect.squareup.com/v1/batch', [
             'headers' => [
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json'
-            ], 'body' => json_encode($newItemRequestBatch)
+            ], 'body' => json_encode($newItemRequestFullBatch)
     ]);
 
-        $newItemList = json_decode($newItemBatch->getBody(), true);
-
-        var_dump($newItemList);
-        # Creates an item with the input values item.
-        /*$itemsRequest = $client->request('POST', 'https://connect.squareup.com/v1/'.$existingLocation[0]['squareID'].'/items', [
-            'headers' => [
-                'Authorization' => 'Bearer '.$access_token ,
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json'
-            ], 'body' => $json
-        ]);
-
-        $itemsContents = $itemsRequest->getBody();
-        $itemsList = json_decode($itemsContents, true);
-
-        array_push($newItemArray, array('itemID' => $itemsList['id'], 'locationID' => $existingLocation[0]['squareID'], 'locationName' => $newSquareItemLocation));*/
-    
-
-    //write get request to make sure I can retrieve all the item corectly
-    /*$getNewItemsMade = [];
-    $catData = json_encode(array('category_id' => '67c8e187-45af-4795-ba56-985f88051453'));
-
-    $batchBody = array('requests' => array(
-                        array('method' => 'PUT',
-                              'relative_path' => '/v1/3526BMVFNJZZX/items/62c7c04e-37b6-44cd-9d11-c7d36ea154ef',
-                              'access_token' => 'KI0ethBHis2N76q1jyYung',
-                              'body' => array('category_id' => 'b60e92d8-97ef-4ebf-a84f-b2b9a695419e')
-                              ),
-                        array('method' => 'PUT',
-                              'relative_path' => '/v1/1H5A5ZGP2T4DA/items/15055252-67a6-4429-ad3a-c7076e56ff39',
-                              'access_token' => 'KI0ethBHis2N76q1jyYung',
-                              'body' => array('category_id' => '67c8e187-45af-4795-ba56-985f88051453')
-                              )
-                        ));
-    $jsonBatchBody = json_encode($batchBody);
-
-    $itemsBatch = $client->request('POST', 'https://connect.squareup.com/v1/batch', [
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json'
-                ], 'body' => $jsonBatchBody
-        ]);
-
-        $itemsContents = $itemsBatch->getBody();
-        $itemsList = json_decode($itemsContents, true);*/
-
-    return 'working';
+    //Time to set up and add to DB new item and update inventory as well as unit price to finish out the request. 
+    return $newItemBatchResponse->getBody();
     exit;
 };
