09186a4223da271461cc48b94145f7eb0512fa76
Kyle Raley <kyle@geteelo.com>
2017-07-31 18:39:02 -0600

    This is the start of user functionality with user permissions

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use User;
+
+class userController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (Auth::check()){//The user is logged in
+            //Show all current managers associated to owner
+
+
+
+
+
+            return view('userProfile');
+        }else{
+            return redirect('/auth/login');
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * This is where the new manager will be created
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *Show details about specific manager
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *Edit details such as email for managers
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *Delete and remove permission from that user
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/userProfile.blade.php
@@ -0,0 +1,18 @@
+@extends('adminLayout')
+
+@section('pagespecificstyles')
+    <!-- vendor page speciic styles-->
+
+@stop
+
+@section('content')
+<!-- Main content -->
+<section class="content">
+THis will be the user profile page
+</section><!-- /.content -->
+@endsection
+
+@section('pagespecificscripts')
+    <!-- purchaseOrder page speciic styles-->
+    <script src="{{ asset ("/js/vendorCenter.js") }}" type="text/javascript"></script>
+@stop
